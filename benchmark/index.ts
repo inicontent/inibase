f7de4130370f028f63e7184fcfd8c602882d4575
kamatil-dev <karim.amahtil@gmail.com>
2024-05-01 13:35:15 +0100

    dev: optimize benchmark

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -8,69 +8,69 @@ const bench = new Bench();
 if (await isExists("test")) await rm("test", { recursive: true });
 
 const db = new Inibase("test");
-let i;
+let i: number;
 await db.setTableSchema("user", [
-  {
-    key: "username",
-    type: "string",
-    required: true,
-  },
-  {
-    key: "email",
-    type: "email",
-    required: true,
-  },
+	{
+		key: "username",
+		type: "string",
+		required: true,
+	},
+	{
+		key: "email",
+		type: "email",
+		required: true,
+	},
 ]);
 
 bench
-  .add(
-    "POST",
-    async () => {
-      await db.post("user", {
-        username: `username_${i}`,
-        email: `email_${i}@test.com`,
-      });
-    },
-    {
-      beforeAll() {
-        i = 0;
-      },
-      beforeEach() {
-        i++;
-      },
-    }
-  )
-  .add(
-    "PUT",
-    async () => {
-      await db.put("user", { username: `edited_${i}` }, i);
-    },
-    {
-      beforeAll() {
-        i = 0;
-      },
-      beforeEach() {
-        i++;
-      },
-    }
-  )
-  .add(
-    "GET",
-    async () => {
-      await db.get("user", i);
-    },
-    {
-      beforeAll() {
-        i = 0;
-      },
-      beforeEach() {
-        i++;
-      },
-    }
-  )
-  .add("DELETE", async () => {
-    await db.delete("user", 1);
-  });
+	.add(
+		"POST",
+		async () => {
+			await db.post("user", {
+				username: `username_${i}`,
+				email: `email_${i}@test.com`,
+			});
+		},
+		{
+			beforeAll() {
+				i = 0;
+			},
+			beforeEach() {
+				i++;
+			},
+		},
+	)
+	.add(
+		"PUT",
+		async () => {
+			await db.put("user", { username: `edited_${i}` }, i);
+		},
+		{
+			beforeAll() {
+				i = 0;
+			},
+			beforeEach() {
+				i++;
+			},
+		},
+	)
+	.add(
+		"GET",
+		async () => {
+			await db.get("user", i);
+		},
+		{
+			beforeAll() {
+				i = 0;
+			},
+			beforeEach() {
+				i++;
+			},
+		},
+	)
+	.add("DELETE", async () => {
+		await db.delete("user", 1);
+	});
 
 await bench.warmup(); // make results more reliable, ref: https://github.com/tinylibs/tinybench/pull/50
 await bench.run();
