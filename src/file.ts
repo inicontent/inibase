ff84dc6bf6abd5b20935778a7cefc83d9db9f704
kamatil <karim.amahtil@gmail.com>
2024-12-10 15:48:54 +0100

    fix: searchIn arg in search()

--- src/file.ts
+++ src/file.ts
@@ -832,7 +832,7 @@ export const search = async (
 			linesCount++;
 
 			// Search only in provided linesNumbers
-			if ((searchIn && !searchIn.has(linesCount)) || searchIn.has(-linesCount))
+			if (searchIn && (!searchIn.has(linesCount) || searchIn.has(-linesCount)))
 				continue;
 
 			// Decode the line for comparison.
